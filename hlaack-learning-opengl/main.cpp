bed5c71d71d9dd842a3b5949145d85d94adf9122
hlaack <harry@spolem.net>
2025-06-10 01:10:26 -0500

    Added clarifying comments
    Reviewed code for understanding, and added comments for contextual learning.

--- hlaack-learning-opengl/main.cpp
+++ hlaack-learning-opengl/main.cpp
@@ -3,21 +3,24 @@
 
 #include <iostream>
 
-
+//FUNCTION TEMPLATES
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+//UNCHANGING INT TYPES THAT CAN ONLY STORE POSITIVE VALUES (large possible values than a signed int)
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 int main() {
 	glfwInit();
+	//SET UPPER AND LOWER VERSION LIMITS
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//CREATE WINDOW VARIABLE
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-	if (window == NULL) {
+	if (window == NULL) { //if we have no window
 		std::cout << "Failed to create GLFW window." << std::endl;
 		glfwTerminate();
 		return -1;
@@ -30,6 +33,7 @@ int main() {
 		return -1;
 	}
 
+	//WHILE THE WINDOW IS OPEN (while bool WindowShouldClose is not true!)
 	while (!glfwWindowShouldClose(window)) {
 		
 		//HANDLE INPUT
@@ -44,14 +48,17 @@ int main() {
 		glfwPollEvents();
 	}
 
+	//CLEAN UP WINDOW
 	glfwTerminate();
 	return 0;
 }
 
+//THIS FUNCTION IS UTILIZED BY glfwSetFramebufferSizeCallback TO CHECK UPDATES TO WINDOW SIZE
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
+//THIS FUNCTION HANDLES ANY INPUT AND IS CALLED CONCURRENTLY WHILE THE WINDOW EXISTS
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
